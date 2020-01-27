65d425afde1359cd5b79e5eb8d8d494c25d4b012
Owen Abbott <ocabbott@gmail.com>
2020-01-27 03:04:37 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,9 +3,9 @@ def square_array(array)
   counter=0
   i = array.length()
   while i > 0 do
-    i -= 1
     new_array.push(array[counter]**2)
-    counter+=1
+    counter += 1
+    i -= 1
   return new_array
 end
 end
